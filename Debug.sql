e303ab4d39f308ae427ea2ae625452eeca6585f7
JimYang2023 <y20041112@gmail.com>
2024-12-23 14:19:37 +0800

    upload create table files

---
+++ Debug.sql
@@ -0,0 +1,11 @@
+select COUNT(*) as benefits_cnt from benefits;
+select COUNT(*) as job_industry_cnt from job_industry;
+select COUNT(*) as salary_cnt from salary;
+select COUNT(*) as job_skill_cnt from job_skill;
+select COUNT(*) as skills_cnt from skills;
+select COUNT(*) as industries_cnt from industries;
+select COUNT(*) as employee_count_cnt from employee_count;
+select COUNT(*) as company_speciality_cnt from company_speciality;
+select COUNT(*) as company_industry_cnt from company_industry;
+select COUNT(*) as company_cnt from company;
+select COUNT(*) as job_posting from job_posting;
