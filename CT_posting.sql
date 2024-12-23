74d744a33ced3ffc940890f286ef595b83558169
JimYang2023 <y20041112@gmail.com>
2024-12-23 22:38:31 +0800

    fix posting table problem

--- CT_posting.sql
+++ CT_posting.sql
@@ -2,39 +2,44 @@ create table job_posting(
     Job_id varchar(10),
     Company_name varchar(50),
     Title varchar(255),
-    Description varchar(255),
-    Max_salary int,
-    Pay_period varchar(50),
-    Location varchar(255),
+    Posting_description varchar(255),
+    Posting_location varchar(255),
     Company_id varchar(10),
     Views int,
-    Med_salary int,
-    Min_salary int,
     Formatted_work_type varchar(50),
     Applies int,
     Original_listed_time bigint,
-    Remote_allowed bigint,
     Job_posting_url varchar(255),
-    Application_url varchar(255),
     Application_type varchar(20),
     Expiry bigint,
-    Closed_time bigint,
-    Formatted_experience_level varchar(255),
-    Skills_desc varchar(50),
     Listed_time bigint,
-    Posting_domain varchar(50),
     Sponsored int,
     Work_type varchar(50),
-    Currency varchar(50),
-    Compensation_type varchar(50),
-    Normalized_salary int,
-    Zip_code int,
-    Fips int,
     primary key (Job_id)
 );
 
-LOAD DATA LOCAL INFILE './archive/postings.csv'
+/*
+job_id,
+company_name,
+title,
+description,
+location,
+company_id,
+views,
+formatted_work_type,
+original_listed_time,
+job_posting_url,
+application_type,
+expiry,
+listed_time,
+posting_domain,
+sponsored,
+work_type
+*/
+
+LOAD DATA LOCAL INFILE 'archive/postings.csv'
 INTO TABLE job_posting
+CHARACTER SET latin1
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n' 
 IGNORE 1 LINES;
